c468d19c89c63050c313cc8817e2932dc67473ec
WaloNeville <neville.walo@gmail.com>
2018-10-13 22:18:05 +0200

    2

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 #include <math.h>
 #include <malloc.h>
 #include <stdlib.h>
-#include <time.h>
 
-int comparefct(int *a, int *b) {
-    return *a < *b;
-};
+int cmpfunc(const void *a, const void *b) {
+    return (*(int *) a - *(int *) b);
+}
 
 int main() {
-    int Max2Pow = 10;
+    int Max2Pow = 20;
     int Repetitions = 1;
     for (int i = 0; i < Max2Pow; ++i) {
         int size = pow(2, i);
@@ -25,14 +24,14 @@ int main() {
                 array[k] = rand();
             }
             clock_t start = clock();
-            qsort(array, size, sizeof(int), comparefct);
+            qsort(array, size, sizeof(int), cmpfunc);
             clock_t end = clock() - start;
-            double timetaken = (double) end / CLOCKS_PER_SEC;
-            TimeSum+=timetaken;
+            double TimeTaken = (double) end / CLOCKS_PER_SEC;
+            TimeSum += TimeTaken;
 
 
         }
-        double averagetime = TimeSum/Repetitions;
-        printf("%e\n",averagetime);
+        double AverageTime = TimeSum / Repetitions;
+        printf("%e\n", AverageTime);
     }
 }
